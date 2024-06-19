4ce5b964ad9014e3572842fcba175395e5c948cd
giwadekunle <giwadekunle@gmail.com>
2024-06-19 12:12:54 +0300

    vuetify

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,17 @@
 <template>
-    
+    <v-app-bar :elevation="2" color ="blue">
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  </template>
+
+  <v-app-bar-title>Camel Mart</v-app-bar-title>
+  <v-btn to ="/">Home</v-btn>
+  <v-btn to="/products">Products</v-btn>
+  <v-btn to="/aboutus">About Us</v-btn>
+  <v-btn to="/service">Services</v-btn>
+  <v-btn to="/cart">Cart</v-btn>
+  <v-btn to="/contactus">Contact Us</v-btn>
+ 
+
+</v-app-bar>
 </template>
